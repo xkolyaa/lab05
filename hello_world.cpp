a7be1f1a75fde47b1cc13395ab50fb4d5d3e5612
Nikolay <nik.chi09@gmail.com>
2026-03-16 17:41:46 +0300

    Refactor: remove using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <string>
-
-using namespace std;                         
-int main()
-{    string name;    
-    cout << "Enter your name: ";
-    cin >> name;    
-    cout << "Hello world from " << name << endl;
-        return 0;}
+int main()                                 
+{    std::string name;
+std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello world from " << name << std::endl;                                           
+    return 0;}                                                         
