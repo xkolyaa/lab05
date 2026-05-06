00996471b69011ebfb6ddbe96b93d8ec8162873e
xkolyaa <nikoachibi@gmail.com>
2026-05-06 09:25:02 +0300

    removed build artifacts and updated .gitignore

--- _build/solver_lib/CMakeFiles/solver_lib.dir/sources/solver.cpp.o.d
+++
@@ -1,4 +0,0 @@
-solver_lib/CMakeFiles/solver_lib.dir/sources/solver.cpp.o: \
- /home/nikolay/project/lab05/solver_lib/sources/solver.cpp \
- /usr/include/stdc-predef.h \
- /home/nikolay/project/lab05/solver_lib/include/solver.h
