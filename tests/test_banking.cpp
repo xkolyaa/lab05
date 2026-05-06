b8cf5d4b4e4c71d21532dc8834f6fdde862f33ff
xkolyaa <nikoachibi@gmail.com>
2026-05-06 09:55:37 +0300

    final: integrated coveralls

--- tests/test_banking.cpp
+++ tests/test_banking.cpp
@@ -11,21 +11,26 @@ public:
     MOCK_METHOD0(Unlock, void());
 };
 
-TEST(Account, BasicMethods) {
+TEST(Account, Methods) {
     Account acc(1, 1000);
     EXPECT_EQ(acc.GetBalance(), 1000);
+    acc.Lock();
+    EXPECT_THROW(acc.ChangeBalance(100), std::runtime_error);
+    acc.Unlock();
+    acc.ChangeBalance(100);
+    EXPECT_EQ(acc.GetBalance(), 1100);
 }
 
 TEST(Transaction, Logic) {
     Transaction tr;
     Account a1(1, 1000), a2(2, 1000);
-    
-    // Тестируем только успешный перевод и нехватку средств
     EXPECT_TRUE(tr.Make(a1, a2, 200));
-    EXPECT_FALSE(tr.Make(a1, a2, 2000));
+    EXPECT_EQ(a1.GetBalance(), 800);
+    EXPECT_EQ(a2.GetBalance(), 1200);
+    EXPECT_FALSE(tr.Make(a1, a2, 5000));
 }
 
-TEST(Account, MockExample) {
+TEST(Account, Mock) {
     MockAccount m_acc(1, 100);
     EXPECT_CALL(m_acc, GetBalance()).WillOnce(testing::Return(100));
     EXPECT_EQ(m_acc.GetBalance(), 100);
